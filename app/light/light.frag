10e8fe9e97bcdf6cce177146f89f07798f7c86b4
pgeowng <pgeowng@gmail.com>
2022-11-11 03:13:21 +0300

    feat: ambient & specular light

--- app/light/light.frag
+++ app/light/light.frag
@@ -1,9 +1,25 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 FragPos;
+in vec3 Normal;
+
+uniform vec3 lightPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 
 void main() {
-    FragColor = vec4(objectColor * lightColor, 1.0);
+    float ambientStrength = 0.1;
+    float ambient = ambientStrength;
+
+
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float lightAngle = dot(normalize(Normal), lightDir);
+    float specular = clamp(lightAngle, 0, 1);
+    // vec3 specular = vec3(1.0) ;
+
+
+    // vec3 result = max(specular, ambient) * lightColor * objectColor; // iteration 1: allow ambient light minimum
+    vec3 result = (specular + ambient) * lightColor * objectColor; // iteration 2: allow always above ambient light
+    FragColor = vec4(result, 1.0);
 }
--- app/light/light.h
+++ app/light/light.h
@@ -7,6 +7,7 @@
 #include <glm/gtc/type_ptr.hpp>
 #include "shader.h"
 #include "flycamera.h"
+#include "debug.h"
 
 struct app_s {
 		// glm::vec3 position;
@@ -44,57 +45,128 @@ bool app_init(app_s *app) {
 	float cube[] = {
 		// front
 		-0.5f, -0.5f, -0.5f,
+		0.0f, 0.0f, -0.5f,
+
 		-0.5f, 0.5f, -0.5f, // Front Left Top
-		0.5f, 0.5f, -0.5f, // Front Right Top
+		0.0f, 0.0f, -0.5f,
 
 		0.5f, 0.5f, -0.5f, // Front Right Top
+		0.0f, 0.0f, -0.5f,
+
+
+		0.5f, 0.5f, -0.5f, // Front Right Top
+		0.0f, 0.0f, -0.5f,
+
 		0.5f, -0.5f, -0.5f, // Front Right Bottom
+		0.0f, 0.0f, -0.5f,
+
 		-0.5f, -0.5f, -0.5f,
+		0.0f, 0.0f, -0.5f,
+
 
 		// right
 		0.5f, -0.5f, -0.5f,
+		1.0f, 0.0f, 0.0f,
+
 		0.5f, 0.5f, -0.5f,
-		0.5f, 0.5f, 0.5f,
+		1.0f, 0.0f, 0.0f,
 
 		0.5f, 0.5f, 0.5f,
+		1.0f, 0.0f, 0.0f,
+
+
+		0.5f, 0.5f, 0.5f,
+		1.0f, 0.0f, 0.0f,
+
 		0.5f, -0.5f, 0.5f,
+		1.0f, 0.0f, 0.0f,
+
 		0.5f, -0.5f, -0.5f,
+		1.0f, 0.0f, 0.0f,
+
 
 		// back
 		0.5f, -0.5f, 0.5f,
+		0.0f, 0.0f, 1.0f,
+
 		0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f, 0.5f,
+		0.0f, 0.0f, 1.0f,
 
 		-0.5f, 0.5f, 0.5f,
+		0.0f, 0.0f, 1.0f,
+
+
+		-0.5f, 0.5f, 0.5f,
+		0.0f, 0.0f, 1.0f,
+
 		-0.5f, -0.5f, 0.5f,
+		0.0f, 0.0f, 1.0f,
+
 		0.5f, -0.5f, 0.5f,
+		0.0f, 0.0f, 1.0f,
+
 
 		// left
 		-0.5f, -0.5f, 0.5f,
+		-1.0f, 0.0f, 0.0f,
+
 		-0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f, -0.5f,
+		-1.0f, 0.0f, 0.0f,
 
 		-0.5f, 0.5f, -0.5f,
+		-1.0f, 0.0f, 0.0f,
+
+		-0.5f, 0.5f, -0.5f,
+		-1.0f, 0.0f, 0.0f,
+
 		-0.5f, -0.5f, -0.5f,
+		-1.0f, 0.0f, 0.0f,
+
 		-0.5f, -0.5f, 0.5f,
+		-1.0f, 0.0f, 0.0f,
+
 
 		// top
 		-0.5f, 0.5f, -0.5f,
+		0.0f, 1.0f, 0.0f,
+
 		-0.5, 0.5f, 0.5f,
-		0.5f, 0.5f, 0.5f,
+		0.0f, 1.0f, 0.0f,
 
 		0.5f, 0.5f, 0.5f,
+		0.0f, 1.0f, 0.0f,
+
+
+		0.5f, 0.5f, 0.5f,
+		0.0f, 1.0f, 0.0f,
+
 		0.5, 0.5f, -0.5f,
+		0.0f, 1.0f, 0.0f,
+
 		-0.5f, 0.5f, -0.5f,
+		0.0f, 1.0f, 0.0f,
+
 
 		// bottom
 		-0.5f, -0.5f, -0.5f,
+		0.0f, -1.0f, 0.0f,
+
 		-0.5, -0.5f, 0.5f,
-		0.5f, -0.5f, 0.5f,
+		0.0f, -1.0f, 0.0f,
 
 		0.5f, -0.5f, 0.5f,
+		0.0f, -1.0f, 0.0f,
+
+
+		0.5f, -0.5f, 0.5f,
+		0.0f, -1.0f, 0.0f,
+
 		0.5, -0.5f, -0.5f,
+		0.0f, -1.0f, 0.0f,
+
 		-0.5f, -0.5f, -0.5f,
+		0.0f, -1.0f, 0.0f
+
 	};
 
 	glGenVertexArrays(1, &app->vao);
@@ -103,16 +175,20 @@ bool app_init(app_s *app) {
 	glBindBuffer(GL_ARRAY_BUFFER, app->vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glGenVertexArrays(1, &app->light_vao);
 	glBindVertexArray(app->light_vao);
 
 	glBindBuffer(GL_ARRAY_BUFFER, app->vbo);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 
 	return ok;
 }
@@ -120,35 +196,45 @@ bool app_init(app_s *app) {
 void app_update(app_s *app, float dt) {
 	flycamera_update(&app->camera, dt);
 
+	 float time = SDL_GetTicks() / 1000.0f;
+
 	glm::mat4 model = glm::mat4(1.0f);
+	glm::vec3 light_pos = glm::vec3(1.2f + cos(time), 1.0f, 2.0f + sin(time));
+
+
+        glm::mat4 view = flycamera_get_view_matrix(&app->camera);
+        glm::mat4 proj = flycamera_get_projection_matrix(&app->camera);
+
 
  {
-	shader_Use(&app->lighting_shader);
-	shader_Set3f(&app->lighting_shader, "objectColor", 1.0f, 0.5f, 0.31f);
-	shader_Set3f(&app->lighting_shader, "lightColor", 1.0f, 1.0f, 1.0f);
-	shader_SetMatrix4fv(&app->lighting_shader, "model", glm::value_ptr(model));
-	shader_SetMatrix4fv(&app->lighting_shader, "view", glm::value_ptr(flycamera_get_view_matrix(&app->camera)));
-	shader_SetMatrix4fv(&app->lighting_shader, "projection", glm::value_ptr(flycamera_get_projection_matrix(&app->camera)));
-
-	glBindVertexArray(app->vao);
-	glBindBuffer(GL_ARRAY_BUFFER, app->vbo);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
- }
-
- {
-	model = glm::translate(model, glm::vec3(1.2f, 1.0f, 2.0f));
+	model = glm::translate(model, light_pos);
+	model = glm::scale(model, glm::vec3(0.2f));
 
 	shader_Use(&app->lamp_shader);
 	shader_SetMatrix4fv(&app->lamp_shader, "model", glm::value_ptr(model));
-	shader_SetMatrix4fv(&app->lamp_shader, "view", glm::value_ptr(flycamera_get_view_matrix(&app->camera)));
-	shader_SetMatrix4fv(&app->lamp_shader, "projection", glm::value_ptr(flycamera_get_projection_matrix(&app->camera)));
+	shader_SetMatrix4fv(&app->lamp_shader, "view", glm::value_ptr(view));
+	shader_SetMatrix4fv(&app->lamp_shader, "projection", glm::value_ptr(proj));
 
 	glBindVertexArray(app->light_vao);
 	glBindBuffer(GL_ARRAY_BUFFER, app->vbo);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glDrawArrays(GL_TRIANGLES, 0, 72);
  }
 
+ {
+	model = glm::mat4(1.0f);
+	shader_Use(&app->lighting_shader);
+	shader_Set3f(&app->lighting_shader, "objectColor", 1.0f, 0.5f, 0.31f);
+	shader_Set3f(&app->lighting_shader, "lightColor", 1.0f, 1.0f, 1.0f);
+	shader_Set3f(&app->lighting_shader, "lightPos", light_pos.x, light_pos.y, light_pos.z);
 
+	shader_SetMatrix4fv(&app->lighting_shader, "model", glm::value_ptr(model));
+	shader_SetMatrix4fv(&app->lighting_shader, "view", glm::value_ptr(view));
+	shader_SetMatrix4fv(&app->lighting_shader, "projection", glm::value_ptr(proj));
+
+	glBindVertexArray(app->vao);
+	glBindBuffer(GL_ARRAY_BUFFER, app->vbo);
+	glDrawArrays(GL_TRIANGLES, 0, 72);
+ }
 }
 
 #endif
